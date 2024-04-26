618a039a3679f9b95a250a788771b010b8258deb
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-04-26 00:38:54 -0300

    Chore: Add new routes for categories and courses

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const users = require('./userRoute.js');
+const categories = require('./categoryRoute.js');
+const courses = require('./courseRoute.js');
 
 module.exports = (app) => {
   app.use(
     express.json(),
-    users
+    users,
+    categories,
+    courses
   );
 }
 
