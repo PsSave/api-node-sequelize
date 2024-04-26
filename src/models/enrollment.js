9d55df3bd408bd32b8defeac9e72c7e133665af3
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-04-25 23:22:23 -0300

    Refactor: model associations in category.js, course.js, enrollment.js, and user.js

--- src/models/enrollment.js
+++ src/models/enrollment.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Enrollment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Enrollment.belongsTo(models.User, {
+        foreignKey: 'studant_id'
+      })
+      Enrollment.belongsTo(models.Course, {
+        foreignKey: 'course_id'
+      })
     }
   }
   Enrollment.init({
--- src/models/user.js
+++ src/models/user.js
@@ -4,13 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      User.hasMany(models.Course, {
+        foreignKey: 'teacher_id'
+      });
+      User.hasMany(models.Enrollment, {
+        foreignKey: 'studant_id'
+      });
     }
   }
   User.init({
